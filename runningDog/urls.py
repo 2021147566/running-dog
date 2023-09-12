5ce79e33d227898ad7502823e21c56634dc30278
solleepang <dlthf727@gmail.com>
2023-09-12 16:20:17 +0900

    필요없는 부분 삭제 및 수정

--- runningDog/urls.py
+++ runningDog/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first/', views.first_view, name='first_view'),
-    path('second/', views.second_view, name='home'),
-    path('signup/', views.sign_up_view, name='home'),
-    path('signin/', views.sign_in_view, name='signin'),
-    path('detail/', views.detail_view, name='signin'),
-    path('mypage/', views.mypage_view, name='mypage'),
+    path('', include('post.urls')),
 ]
