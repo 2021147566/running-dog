3062f9d2eed7277521019cc7e63107ce05c2fe7d
solleepang <dlthf727@gmail.com>
2023-09-12 16:18:45 +0900

    유저모델을 AbstractUser 확장 모델로 변경

--- user/models.py
+++ user/models.py
@@ -1,14 +1,11 @@
 # user/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class UserModel(models.Model):
+class UserModel(AbstractUser):
     class Meta:
         db_table = "my_user"
 
-    username = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=256, null=False)
     bio = models.CharField(max_length=256, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
